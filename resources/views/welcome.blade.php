e7183fbda5aac24038f0d9aa977a50fd25bdc5fd
ryuzo.takagi <william_billl2008@yahoo.co.jp>
2019-07-12 16:19:42 +0000

    user pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>大阪ラーメンまとめTOP</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-info']) !!}
         </div>
     </div>
 @endsection
